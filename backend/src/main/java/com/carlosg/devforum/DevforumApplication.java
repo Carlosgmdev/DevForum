8ea14be5f860337c00f95f76b76f55788b7f76e9
Carlosgmdev <m_ccg@outlook.com>
2023-09-04 15:51:49 -0600

    "Adding backend files"

---
+++ backend/src/main/java/com/carlosg/devforum/DevforumApplication.java
@@ -0,0 +1,13 @@
+package com.carlosg.devforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevforumApplication.class, args);
+	}
+
+}
