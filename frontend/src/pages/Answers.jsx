76e0df498cfa51ff572a41a3a57e59a9ec62b938
Carlosgmdev <m_ccg@outlook.com>
2023-09-07 01:12:00 -0600

    Sync

--- frontend/src/pages/Answers.jsx
+++ frontend/src/pages/Answers.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import TopicCard from '../components/TopicCard';
